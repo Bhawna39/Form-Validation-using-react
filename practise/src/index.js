2427ad82cdc4579a1febd6a1559f0aec85f9a95f
Bhawna <92748048+Bhawna39@users.noreply.github.com>
2023-05-11 12:14:46 +0530

    submitting code-Thu May 11 2023 12:14:42 GMT+0530 (India Standard Time)

---
+++ practise/src/index.js
@@ -0,0 +1,8 @@
+import React,{useState,useEffect}from 'react';
+const Covid19= () => {
+  const [country, setCountry]= useState("");
+  const [resp ,setResp] = useState([]);
+  useEffect(() => {
+    fetch()
+  })
+}
